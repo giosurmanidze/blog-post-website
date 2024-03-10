cba0b2f3538fa212955119b3a37c3d334c50696e
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-10 13:42:34 +0400

    feat/post repository

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+namespace App\Repositories;
+
+use App\Contracts\PostRepositoryInterface;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Collection;
+
+class PostRepository implements PostRepositoryInterface {
+
+    public function getAll(): Collection
+    {
+        return Post::all();
+    }
+
+    public function getById($id): ?Post
+    {
+        return Post::findOrFail($id);
+    }
+
+}
